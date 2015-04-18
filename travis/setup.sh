7193b40165edeff498fce18f2f159cc1086c5652
kari <mr.kari.ikonen@gmail.com>
2015-04-18 11:32:32 +0300

    Try to remove extra bundle install

--- travis/setup.sh
+++ travis/setup.sh
@@ -1,12 +1,3 @@
-echo "================================"
-ls -al vendor/bundle
-echo "================================"
-
-cd spec/dummy
-
-export BUNDLE_GEMFILE=$PWD/Gemfile
-bundle install
-
-echo "================================"
-ls -al vendor/bundle
-echo "================================"
+#cd spec/dummy
+#export BUNDLE_GEMFILE=$PWD/Gemfile
+#bundle install
